23184af2be7f4e48da764be47bd3e0609243c40f
Dixon Minnick <dewey.moor206@gmail.com>
2013-04-11 20:37:18 -0400

    changing some shit ;)

--- poop/poop.js
+++ poop/poop.js
@@ -3,6 +3,12 @@ function init(){
 	var poop = new Object();
 	poop = {"game_title" : "poop" , "username" : "catherine" , "score" : 69 };
 	var mukmuk = JSON.stringify(poop);
-	$.post("http://gentle-shore-6564.herokuapp.com/poop.json", mukmuk);
+	$.post("http://gentle-shore-6564.herokuapp.com/submit.json", mukmuk);
 	
+	taco = $.get("http://gentle-shore-6564.herokuapp.com/highscores.json", function() {
+  alert("success");
+})
+.done(function() { alert("second success"); })
+.fail(function() { alert("error"); })
+.always(function() { alert("finished"); });
 }
