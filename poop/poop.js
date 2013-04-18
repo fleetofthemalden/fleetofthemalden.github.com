3b9c61d0afff0737c3a02c5e2c2980a97810232f
Dixon Minnick <dewey.moor206@gmail.com>
2013-04-17 21:57:41 -0400

    changing poop json format

--- poop/poop.js
+++ poop/poop.js
@@ -2,7 +2,7 @@ var taco;
 var mukmuk;
 function init(){
 	var poop = new Object();
-	poop = {"game_title":"poop","username":"catherine","score":69};
+	poop = {game_title:"poop", username:"cat_mad", score:99};
 	mukmuk = JSON.stringify(poop);
 	$.post("http://gentle-shore-6564.herokuapp.com/submit.json", poop);
 }
